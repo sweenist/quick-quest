90624a5ef1d09131c53cb492170e09a682b86cb7
Ryan Sweeney <sween119@hotmail.com>
2026-01-02 07:58:24 -0500

    reverting camera event extension

--- src/Scenes/baselevel.ts
+++ src/Scenes/baselevel.ts
@@ -2,18 +2,6 @@ import { Camera, CameraEvents, Engine, GameEvent, Scene } from "excalibur";
 import { Player } from "../Actors/player";
 import { Dialog } from "../Dialog/dialog";
 
-declare module 'excalibur' {
-  interface CameraEvents {
-    zoomchanged: ZoomChangeEvent
-  }
-}
-
-class ZoomChangeEvent extends GameEvent<Camera> {
-  constructor(self: Camera) {
-    super();
-    this.target = self;
-  }
-}
 
 export class BaseLevel extends Scene {
   public dialog!: Dialog;
@@ -26,10 +14,8 @@ export class BaseLevel extends Scene {
       maxFrameHeight: 144
     });
 
-    this.camera.events.on(CameraEvents.zoomchanged)
-
     this.camera.strategy.lockToActor(player);
-    this.camera.zoom = 2;
+    // this.camera.zoom = 2;
 
     this.add(this.dialog);
   }
