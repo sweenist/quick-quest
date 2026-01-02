d4f16c075db0d4cfb1245fd491141e1d72cdfed9
Ryan Sweeney <sween119@hotmail.com>
2026-01-01 22:15:22 -0500

    configure dialog box innards

--- src/Dialog/dialog.ts
+++ src/Dialog/dialog.ts
@@ -29,6 +29,8 @@ interface DialogConfig {
   transitionInSpeed?: number;
   transitionOutSpeed?: number;
   textSpeed?: number;
+  textSize?: number;
+  textLineHeight?: number;
   portraitMargin?: number;
   margin: number;
 }
@@ -46,7 +48,9 @@ export class Dialog extends ScreenElement {
   growthRate: number = 12;
   transitionInSpeed: number;
   transitionOutSpeed: number;
-  textSpeed: number
+  textSpeed: number;
+  textSize: number;
+  textLineHeight: number;
   margin: number;
 
   constructor(config: DialogConfig & ActorArgs) {
@@ -59,6 +63,8 @@ export class Dialog extends ScreenElement {
     this.transitionInSpeed = config.transitionInSpeed ?? 12;
     this.transitionOutSpeed = config.transitionOutSpeed ?? this.transitionInSpeed;
     this.textSpeed = config.textSpeed ?? 25;
+    this.textSize = config.textSize ?? 16;
+    this.textLineHeight = config.textLineHeight ?? 24;
 
     this.frameConfig = {
       source: config.frameSource ?? Resources.DialogFrame,
@@ -80,11 +86,10 @@ export class Dialog extends ScreenElement {
 
   onInitialize(engine: Engine): void {
     conley.on(DialogEvents.ShowDialog, (ev) => {
-      const { camera } = this.scene!;
-      const portraitSize = 64 / camera.zoom;
+      const portraitSize = 64;
       const portraitOffset = portraitSize / 2;
       this.frameState = 'start_growing';
-      this.pos = vec(this.margin, engine.canvas.height / camera.zoom);
+      this.pos = vec(this.margin, engine.screen.height);
       console.info('dialog', this.pos);
       this.portrait = new DialogPortrait({
         portraitGraphic: new Rectangle({
@@ -95,13 +100,11 @@ export class Dialog extends ScreenElement {
       });
       this.addChild(this.portrait);
     });
-
-    if (this.scene)
-      this.growthRate = 12 / this.scene.camera.zoom;
   }
 
   update(engine: Engine, elapsed: number): void {
-    const { input, canvas } = engine;
+    const { input } = engine;
+
     if (input.keyboard.wasPressed(Keys.Space) && this.frameState === 'open') {
       if (this.text?.isDone) {
         this.frameState = 'shrinking';
@@ -114,11 +117,10 @@ export class Dialog extends ScreenElement {
       }
     }
 
-    const { camera } = this.scene!;
     switch (this.frameState) {
       case 'start_growing': {
         this.frameState = 'growing';
-        this.frame.setTargetWidth((canvas.width / camera.zoom) - this.margin, true);
+        this.frame.setTargetWidth((engine.screen.width) - this.margin * 2, true);
         this.frame = this.frame.clone();
         this.graphics.use(this.frame);
         break;
@@ -126,9 +128,9 @@ export class Dialog extends ScreenElement {
       case 'growing': {
         this.frame.setTargetHeight(this.frame.height + this.transitionInSpeed, true);
         this.pos.y -= this.transitionInSpeed
-        if (this.frame.height >= (this.maxFrameHeight / camera.zoom)) {
+        if (this.frame.height >= this.maxFrameHeight) {
           this.frameState = 'done_opening';
-          this.frame.setTargetHeight(this.maxFrameHeight / camera.zoom, true);
+          this.frame.setTargetHeight(this.maxFrameHeight, true);
         }
         this.frame = this.frame.clone();
         this.graphics.use(this.uiGroup);
@@ -137,7 +139,7 @@ export class Dialog extends ScreenElement {
       case 'done_opening': {
         this.frameState = 'open';
         this.portrait?.show();
-        const typeWriterConfig = this.buildTypeWriterConfig("This is some sample text. You are in the throne room\n        \nLine break", camera.zoom);
+        const typeWriterConfig = this.buildTypeWriterConfig("This is some sample text. You are in the throne room\n        \nLine break", engine.screen.width);
         this.text = new TypeWriter(typeWriterConfig);
 
         this.uiGroup.members.push(this.text);
@@ -163,19 +165,19 @@ export class Dialog extends ScreenElement {
     }
   }
 
-  buildTypeWriterConfig(text: string, zoomLevel: number): TypeWriterConfig {
+  buildTypeWriterConfig(text: string, width: number): TypeWriterConfig {
     return {
       text,
-      typeDelay: 18,
+      typeDelay: this.textSpeed,
       textConfig: {
         align: 'left',
         vAlign: 'top',
-        height: 160 / zoomLevel,
-        lineHeight: 24 / zoomLevel,
-        width: 584 / zoomLevel,
-        x: this.portrait ? 120 / zoomLevel : 24 / zoomLevel,
-        y: 24 / zoomLevel,
-        fontSize: 16 / zoomLevel,
+        height: this.maxFrameHeight,
+        lineHeight: this.textLineHeight,
+        width: width - (this.margin * 2),
+        x: this.textSize,
+        y: this.textSize,
+        fontSize: this.textSize,
         font: 'RetroText'
       }
     }
